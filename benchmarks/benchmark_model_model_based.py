e7fec461ce60b0c71925399ea0b79bfd4782fb43
hyungjun <khjmove@snu.ac.kr>
2025-07-07 00:30:07 +0900

    make benchmark_model_model_based.py
    copy benchmark_model

---
+++ benchmarks/benchmark_model_model_based.py
@@ -0,0 +1,265 @@
+import argparse
+import gc
+import pprint
+import numpy as np
+import torch
+import time
+
+import deploy.transformers.modeling_llama as modeling_llama
+import torch
+import transformers
+
+model_configs = [
+    "./modelzoo/llama-2/llama-2-7b",
+    # "./modelzoo/llama-2-13b",
+    #"./modelzoo/llama-3/llama-3-8b", 
+]
+
+benchmark_dtypes = ["int4", torch.float16]
+num_warmup_steps = 2
+num_bench_steps = 1
+
+def repeated_run(num_repeats=10):
+    def func(module):
+        def _f(*args, **kwargs):
+            times = []
+            for i in range(num_repeats):
+                times.append(module(*args, **kwargs, repeat_idx = i))
+            return tuple(zip(*times))
+        return _f
+    return func
+
+def _cleanup():
+    gc.collect()
+    torch.cuda.empty_cache()
+
+@repeated_run()
+def module_benchmark(module, repeat_idx):
+    # warmup
+    for i in range(num_warmup_steps):
+        if repeat_idx == 0 and i == 0:
+            before_forward = print_gpu_memory("before forward")
+        out = module()
+        if repeat_idx == 0 and i == 0:
+            after_forward = print_gpu_memory("after forward")
+            peak = torch.cuda.max_memory_allocated() / 1024**3
+            print(f"Peak memory during forward: {peak:.2f} GB")
+            print(f"for forward without model: {(peak - before_forward):.2f} GB")
+    torch.cuda.synchronize()
+    
+    _cleanup()
+    torch.cuda.reset_max_memory_allocated()
+    start_time = time.perf_counter()
+    
+    
+    for i in range(num_bench_steps):
+        out = module()
+    torch.cuda.synchronize()
+    peak_memory = torch.cuda.max_memory_allocated()
+
+    end_time = time.perf_counter()
+
+    return (end_time - start_time) * 1000 / num_bench_steps, peak_memory
+
+
+def get_model_quantized(args, config_name):
+    config = transformers.AutoConfig.from_pretrained(
+        config_name,
+        attn_implementation="flash_attention_2"
+    )
+    dtype_old = torch.get_default_dtype()
+    torch.set_default_dtype(torch.float16)
+    with transformers.modeling_utils.no_init_weights(): 
+        model = modeling_llama.FlatQuantLlamaForCausalLM(args=args, config=config)
+    torch.set_default_dtype(dtype_old)
+    return model
+
+
+def get_model_hf(config_name):
+    return transformers.LlamaForCausalLM.from_pretrained(
+        config_name, 
+        torch_dtype=torch.float16, 
+        attn_implementation="flash_attention_2"
+    )
+
+def get_model_fp16(config_name):
+    return modeling_llama.FlatQuantFP16LlamaForCausalLM.from_pretrained(
+        config_name, 
+        torch_dtype=torch.float16, 
+        attn_implementation="flash_attention_2"
+    )
+
+
+def run_prefill(model, bsz, prefill_length):
+    device = model.device
+    test_input = torch.randint(100, 200, (bsz, prefill_length), dtype=torch.int32, device=device)
+    return module_benchmark(lambda: model(test_input))
+
+
+def run_decode(model, bsz, prefill_length, decode_steps):
+    device = model.device
+    test_input = torch.randint(100, 200, (bsz, prefill_length), dtype=torch.int32, device=device)
+    model._expected_max_length = prefill_length + decode_steps
+    out = model(test_input)
+    past_key_values = out.past_key_values
+    del out
+    _cleanup()
+    next_input = torch.tensor([[100] for _ in range (bsz)], dtype=torch.int32, device=device)
+    def _decode_for_multiple_steps():
+        past_key_values.length = prefill_length
+        for _ in range(decode_steps):
+            model(next_input, past_key_values=past_key_values)
+    return module_benchmark(_decode_for_multiple_steps)
+    
+
+def run_e2e(model, bsz, prefill_length, decode_steps):
+    device = model.device
+    test_input = torch.randint(100, 200, (bsz, prefill_length), dtype=torch.int32, device=device)
+    next_input = torch.tensor([[100] for _ in range (bsz)], dtype=torch.int32, device=device)
+    def _prefill_and_decode_for_multiple_steps():
+        model._expected_max_length = prefill_length + decode_steps
+        out = model(test_input)
+        for _ in range(decode_steps):
+            model(next_input, past_key_values=out.past_key_values)
+    return module_benchmark(_prefill_and_decode_for_multiple_steps)
+
+
+def _wait_for_input():
+    print("Press enter")
+    input()
+
+@torch.no_grad
+def run_all_for_model(model, bsz, prefill, decode):
+    model.eval()
+    model = model.cuda()
+    time_prefill, _ = run_prefill(model, bsz, prefill)
+    _cleanup()
+    if decode is not None:
+        time_decode, memory_decode = run_decode(model, bsz, prefill, decode)
+        _cleanup()
+        time_e2e, _ = run_e2e(model, bsz, prefill, decode)
+        _cleanup()
+    else:
+        time_decode = time_e2e = None
+    return time_prefill, time_decode, time_e2e, memory_decode
+
+
+def print_e2e_time(args, time_prefill_i4, time_decode_i4, time_e2e_i4, time_prefill_f16, time_decode_f16, time_e2e_f16,
+                   time_prefill_i4_benchmark=None, time_decode_i4_benchmark=None, time_e2e_i4_benchmark=None):
+    prefill_speedup = np.mean(time_prefill_f16) / np.mean(time_prefill_i4)
+    prefill_benchmark_speedup = np.mean(time_prefill_f16) / np.mean(time_prefill_i4_benchmark) if time_prefill_i4_benchmark is not None else None
+    print(f"Prefill time: {np.mean(time_prefill_i4):.3f} +- {1.96 * np.std(time_prefill_i4):.3f}ms\n"
+            f"Speedup: {prefill_speedup:.3f}"
+            + (f" Speedup loss: {prefill_benchmark_speedup - prefill_speedup:.3f}" if time_prefill_i4_benchmark is not None else ""))
+    if args.decode_steps is not None and args.decode_steps != 0:
+        decode_speedup = np.mean(time_decode_f16) / np.mean(time_decode_i4)
+        decode_benchmark_speedup = np.mean(time_decode_f16) / np.mean(time_decode_i4_benchmark) if time_decode_i4_benchmark is not None else None
+        print(f"Decode time: {np.mean(time_decode_i4):.3f} +- {1.96 * np.std(time_decode_i4):.3f}ms\n"
+                f"Speedup: {decode_speedup:.3f}"
+                + (f" Speedup loss: {decode_benchmark_speedup - decode_speedup:.3f}" if time_decode_i4_benchmark is not None else ""))
+        e2e_speedup = np.mean(time_e2e_f16) / np.mean(time_e2e_i4)
+        e2e_benchmark_speedup = np.mean(time_e2e_f16) / np.mean(time_e2e_i4_benchmark) if time_e2e_i4_benchmark is not None else None
+        print(f"E2E time: {np.mean(time_e2e_i4):.3f} +- {1.96 * np.std(time_e2e_i4):.3f}ms\n"
+                f"Speedup: {e2e_speedup:.3f}"
+                + (f" Speedup loss: {e2e_benchmark_speedup - e2e_speedup:.3f}" if time_e2e_i4_benchmark is not None else ""))
+
+## print allocated gpu memory
+def print_gpu_memory(prefix=""):
+    allocated = torch.cuda.memory_allocated() / (1024 ** 3)
+    print(f"{prefix} GPU memory - Allocated: {allocated:.2f} GB")
+    return allocated
+
+def benchmark(args):
+    for config_name in model_configs:
+        pprint.pprint(vars(args))
+
+        # FP16
+        args.fuseLN, args.trans = False, "none"
+        args.online_trans = set()
+        print_gpu_memory("before load model")
+        model = get_model_fp16(config_name)
+        model.to('cuda')
+        print_gpu_memory("after load model")
+        time_prefill_f16, time_decode_f16, time_e2e_f16, mem_f16 = run_all_for_model(
+            model, args.batch_size, args.prefill_seq_len, args.decode_steps)
+        print_gpu_memory("after run_all")
+        del model
+        _cleanup()
+        print(f'------------------------- FP16 ------------------------')
+        print(f"Prefill time: {np.mean(time_prefill_f16):.3f} +- {1.96 * np.std(time_prefill_f16):.3f}ms")
+        if args.decode_steps is not None and args.decode_steps != 0:
+            print(f"Decode time: {np.mean(time_decode_f16):.3f} +- {1.96 * np.std(time_decode_f16):.3f}ms")
+            print(f"E2E time: {np.mean(time_e2e_f16):.3f} +- {1.96 * np.std(time_e2e_f16):.3f}ms")
+
+        # Int4
+        print(f'------------------------- Int4 ------------------------')
+        args.fuseLN, args.trans = False, "none"
+        args.online_trans = set()
+        model = get_model_quantized(args, config_name)
+        time_prefill_i4_benchmark, time_decode_i4_benchmark, time_e2e_i4_benchmark, mem_i4 = run_all_for_model(
+            model, args.batch_size, args.prefill_seq_len, args.decode_steps)
+        del model
+        _cleanup()
+        print_e2e_time(args, time_prefill_i4_benchmark, time_decode_i4_benchmark, time_e2e_i4_benchmark,
+                       time_prefill_f16, time_decode_f16, time_e2e_f16)
+        
+        # QuaRot
+        args.fuseLN, args.trans = True, "had"
+        online_trans_list = [
+            {"o_proj", "qk", "down_proj"}
+        ]
+        for online_trans in online_trans_list:
+            print(f"------------------------- Int4 QuaRot ({'+'.join(online_trans)}) ------------------------")
+            args.online_trans = online_trans
+            model = get_model_quantized(args, config_name)
+            time_prefill_i4, time_decode_i4, time_e2e_i4, mem_i4 = run_all_for_model(
+                model, args.batch_size, args.prefill_seq_len, args.decode_steps)
+            del model
+            _cleanup()
+            print_e2e_time(args, time_prefill_i4, time_decode_i4, time_e2e_i4,
+                           time_prefill_f16, time_decode_f16, time_e2e_f16,
+                           time_prefill_i4_benchmark, time_decode_i4_benchmark, time_e2e_i4_benchmark)
+            
+        # FlatQuant
+        args.fuseLN, args.trans = False, "matmul"
+        online_trans_list = [
+            {"qk", "o_proj", "down_proj", "qkv_proj", "up_gate_proj"}
+        ]
+        for online_trans in online_trans_list:
+            print(f"------------------------- Int4 FlatQuant ({'+'.join(online_trans)}) ------------------------")
+            args.online_trans = online_trans
+            model = get_model_quantized(args, config_name)
+            time_prefill_i4, time_decode_i4, time_e2e_i4, mem_i4 = run_all_for_model(
+                model, args.batch_size, args.prefill_seq_len, args.decode_steps)
+            del model
+            _cleanup()
+            print_e2e_time(args, time_prefill_i4, time_decode_i4, time_e2e_i4,
+                           time_prefill_f16, time_decode_f16, time_e2e_f16,
+                           time_prefill_i4_benchmark, time_decode_i4_benchmark, time_e2e_i4_benchmark)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--batch_size', type=int,
+        help='Batch size',
+        default=None,
+    )
+    parser.add_argument(
+        '--prefill_seq_len', type=int,
+        help='Size of the input sequence',
+        default=2048,
+    )
+    parser.add_argument(
+        '--decode_steps', type=int,
+        help='Decode steps',
+        default=256,
+    )
+    
+    args = parser.parse_args()
+    if args.batch_size is None:
+        for bsz in [1, 2, 4, 8, 16, 32, 64]:
+            args.batch_size = bsz
+            benchmark(args)
+    else:
+        benchmark(args)
