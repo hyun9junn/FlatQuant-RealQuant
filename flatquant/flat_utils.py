65d06bafb7333c6d533dcc9ff86053450e37c7e1
hyungjun <khjmove@snu.ac.kr>
2025-07-08 10:34:29 +0900

    add some comment

--- flatquant/flat_utils.py
+++ flatquant/flat_utils.py
@@ -22,7 +22,7 @@ def reparameterize_ln(ln, trans):
     ln_weight = ln.weight.data
     ori_dtype = ln_weight.dtype
     ln_weight = ln_weight.to(torch.float64)
-    ln_weight = ln_weight * trans.diag_scale.to(torch.float64)
+    ln_weight = ln_weight * trans.diag_scale.to(torch.float64) ## activation에서 on-line으로 하는 대신 ln, upgate weight에 fuse / 논문이랑 달리 diag_scale < 1, 즉 activation엔 곱하고 weight엔 나눔
     ln.weight.data = ln_weight.to(ori_dtype)
     trans.use_diag = False
 
